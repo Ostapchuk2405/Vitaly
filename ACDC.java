5828924b0a838d6168ef8b883bda42d5e73c423f
Ostapchuk2405 <76535132+Ostapchuk2405@users.noreply.github.com>
2020-12-29 09:06:59 +0200

    Add files via upload

---
+++ ACDC.java
@@ -0,0 +1,14 @@
+
+public class ACDC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A, C, D, ACDC;
+		A = 10;
+		C = 12;
+		D = 13;
+		ACDC = (int) (Math.pow(16 , 0) * C + Math.pow(16 , 1) * D + Math.pow(16 , 2) * C + Math.pow(16 , 3) * A ) ;
+		System.out.println("Hexadecimal name of the rock band - ACDC encoded is " + ACDC);
+	}
+
+}
