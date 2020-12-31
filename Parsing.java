9e13a56c8be70541abbd4204c060b9619b509793
Ostapchuk2405 <76535132+Ostapchuk2405@users.noreply.github.com>
2020-12-31 22:02:39 +0200

    Add files via upload

---
+++ Parsing.java
@@ -0,0 +1,13 @@
+package com.itbulls.LearnIT.Ostapchuk.operations;
+
+public class Parsing {
+
+	public static void main(String[] args) {
+
+		int[] number = new int[2];
+		int firstNumber = number[0];
+		int secondNumber = number[1];
+		System.out.println(firstNumber + secondNumber);
+	}
+
+}
