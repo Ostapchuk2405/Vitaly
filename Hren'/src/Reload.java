364e2f45e60a8e253d8513dffd03b77b35213090
Vitaliy <ostapchuk.vitali@gmail.com>
2021-01-07 23:21:07 +0200

    init

---
+++ Hren'/src/Reload.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Reload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] users = new String[] { "admin", "guest" };
+		System.out.println(Arrays.toString(users));
+		if (users[0].contains("admin") && users[1].contains("guest")) {
+			System.out.println("Choose your user:");
+			System.out.println("To choose admin click 0 and guest click 1");
+		}
+		Scanner sc = new java.util.Scanner(System.in);
+		int choose = sc.nextInt();
+		switch (choose) {
+		case (0):
+			System.out.println("Hello, Admin!");
+			break;
+		case (1):
+			System.out.println("Hello, Guest!");
+			break;
+		default:
+			System.out.println("Please, make your choice");
+			
+		}	
+	
+		sc.close();
+	}
+
+	}
+
